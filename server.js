d1ec45a4649c69b2977c91dcdf9b1e787a6d1169
Likhith Gowda <vikasgowdaviki1357@gmail.com>
2026-02-19 23:05:02 +0530

    update CORS for render frontend

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors({
   origin: [
     "http://localhost:5500",
     "http://127.0.0.1:5500",
-    "https://community-sharing.netlify.app"
+    "https://sharehub-frontend.onrender.com"
   ],
   credentials: true
 }));
